d356e05d29907b06dee2ad74541229e0def24e6d
Sonam Sharma <curiousvidhi618@gamil.com>
2021-10-21 10:43:11 +0530

    Dance Website

---
+++ app.js
@@ -0,0 +1,26 @@
+const express =require("express");
+const path=require("path");
+const app= express();
+const port=8000;
+// express static stuff
+app.use('/static',express.static('static')); //For serving static files
+app.use(express.urlencoded());
+
+//PUG specific stuff
+app.set('view engine', 'pug'); //set the template engine as pug
+app.set('views',path.join(__dirname,'views')); //set the views directory
+
+// ENDPOINTS
+app.get('/',(req,res)=>{
+    const params={ };
+    res.status(200).render('home.pug',params);
+});
+app.get('/contact',(req,res)=>{
+    const params={ };
+    res.status(200).render('contact.pug',params);
+});
+
+//START THE SERVER
+app.listen(port,()=>{
+    console.log(`The application started successfully on the port ${port}`);
+});
